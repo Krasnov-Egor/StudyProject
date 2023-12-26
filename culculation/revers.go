b491d9eda39b8d108167487ce82a16a577ef5328
Krasnov-Egor <smr.creativchik@gmail.com>
2023-12-26 18:19:51 +0400

    Chto-to

--- culculation/revers.go
+++ culculation/revers.go
@@ -1,13 +1,13 @@
-package culculation
-
-func GetReverseOrder(a int) []int {
-	var k []int
-
-	for a > 0 {
-		var b int = a % 10
-		k = append(k, b)
-		a = a / 10
-	}
-	return k
-
-}
+//package culculation
+//
+//func GetReverseOrder(a int) []int {
+//	var k []int
+//
+//	for a > 0 {
+//		var b int = a % 10
+//		k = append(k, b)
+//		a = a / 10
+//	}
+//	return k
+//
+//}
